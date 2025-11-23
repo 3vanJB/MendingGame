d1d0f73166a1700d9e749f4a6beef561158ef1ca
3vanJB <163084404+3vanJB@users.noreply.github.com>
2025-11-22 16:09:42 -0800

    Revert "Merge branch 'Dev' into Evan"
    This reverts commit fd9d4b0725ad948a741fefcd73c6de94a470c11e, reversing
    changes made to e663ec027360e4aa89fbc076d795b3dc70fc4fe1.

--- mending-game/Scripts/transition_button.gd
+++
@@ -1,8 +0,0 @@
-extends TextureButton
-@export var changeScene : String
-@export var textString : String
-
-func _ready() -> void:
-	$Label.text = textString
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file(changeScene)
