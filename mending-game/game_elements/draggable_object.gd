dc387b28179315ca80bc5069d9fbef59e1be2847
Jason A. Januar <jason.kwee98@gmail.com>
2025-11-22 17:56:05 -0800

    add draggable objects

---
+++ mending-game/game_elements/draggable_object.gd
@@ -0,0 +1,41 @@
+class_name DraggableObject
+extends Area2D
+
+var sprite: Sprite2D
+var collision: CollisionShape2D
+var type: int = 0
+var dragging = false
+var diff: Vector2 = Vector2.ZERO
+var mouse_inside_area: bool = false
+
+static func new_object(
+	object_sprite: Sprite2D,
+	object_collision: CollisionShape2D,
+	object_type:int = 0
+	) -> DraggableObject:
+	var this_scene: PackedScene = preload("res://game_elements/draggable_object.tscn")
+	var object: DraggableObject = this_scene.instantiate()
+	object.sprite = object_sprite
+	object.add_child(object.sprite)
+	object.collision = object_collision
+	object.add_child(object.collision)
+	object.type = object_type
+	return object
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		diff = self.global_position - get_viewport().get_mouse_position()
+
+func _process(delta: float) -> void:
+	if mouse_inside_area && Input.is_action_pressed("click") && not dragging:
+			dragging = true
+	elif Input.is_action_just_released("click"):
+			dragging = false
+	if dragging:
+		self.position = get_viewport().get_mouse_position() + diff
+
+func _on_mouse_shape_entered(_shape_idx: int) -> void:
+	mouse_inside_area = true
+
+func _on_mouse_shape_exited(_shape_idx: int) -> void:
+	mouse_inside_area = false
--- mending-game/minigames/drag_and_drop.gd
+++ mending-game/minigames/drag_and_drop.gd
@@ -1,1 +1,17 @@
 extends Node
+
+
+func _on_ready() -> void:
+	get_viewport().physics_object_picking_sort = true
+	get_viewport().physics_object_picking_first_only = true
+	var sprite = Sprite2D.new()
+	sprite.texture = load("res://icon.svg")
+	
+	var shape = RectangleShape2D.new()
+	shape.size = sprite.texture.get_size()
+	
+	var collision = CollisionShape2D.new()
+	collision.shape = shape
+	
+	var dragObj = DraggableObject.new_object(sprite, collision)
+	add_child(dragObj)
