b73fd18f200db1c454a9c230d35a9ff0f3b542ed
mkdir2020 <59682958+mkdir2020@users.noreply.github.com>
2025-11-22 22:26:32 -0800

    update

--- mending-game/game_play.gd
+++ mending-game/game_play.gd
@@ -12,20 +12,23 @@ func _ready() -> void:
 	#pass # Replace with function body.
 	
 	$btnPauseContainer.hide()
-	$btnLostContainer.hide()
-	
-	
+	$btnLostGoNextContainer.hide()
+	$btnNoLivesContainer.hide()
+	$lblLostMsg.hide()
+	$lblWinMsg.hide()
+		
 	#Engine.time_scale = 0.2 
 	$timerLabel.text = "%02d:%02d" % [m, s]
 	get_node("Timer").start()
 	
 	update_lives_display()  # show initial lives
-	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 
 
 func _on_timer_timeout() -> void:
@@ -45,8 +48,13 @@ func _on_timer_timeout() -> void:
 
 	if time_left == 0:
 		$Timer.stop()
-		$btnLostContainer.show()
+		# Decrease lives
+		lives -= 1
+		update_lives_display()
+		$lblLostMsg.show()
+		$btnLostGoNextContainer.show()
 		print("Timer finished!")
+		
 
 
 
@@ -58,6 +66,12 @@ func _on_btn_pause_pressed() -> void:
 	print ("Pause")
 
 
+
+func update_lives_display() -> void:
+	$lifeContainer/lifeNumber.text = str(lives)
+
+
+
 func _on_exit_pressed() -> void:
 	#pass # Replace with function body.
 	
@@ -65,13 +79,14 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
+
 func _on_resume_pressed() -> void:
 	#pass # Replace with function body.
+	
 	get_tree().paused = false
 	$btnPauseContainer.hide()
 	print ("Resume")
-	
-	
+
 
 
 func _on_restart_pressed() -> void:
@@ -81,22 +96,30 @@ func _on_restart_pressed() -> void:
 	get_tree().change_scene_to_file("res://game_play.tscn")
 	
 	print ("Restart")
-	
 
 
-func _on_exit2_pressed() -> void:
+
+func _on_back_pressed() -> void:
 	#pass # Replace with function body.
-		
-	print ("Exit after lost")
-	get_tree().quit()
+	
+	get_tree().change_scene_to_file("res://game_play.tscn")
 
 
-func _on_restart2_pressed() -> void:
+func _on_nextGame_pressed() -> void:
+	#pass # Replace with function body.
+
+	get_tree().change_scene_to_file("res://minigames/click_random_buttons.tscn")
+
+
+# =========================================
+
+# Restart level when time reaches 0
+func _on_restartLevel_pressed() -> void:
 	#pass # Replace with function body.
 	
 	$btnLostContainer.hide()
 	
-		# Decrease lives
+	# Decrease lives
 	lives -= 1
 	update_lives_display()
 
@@ -119,7 +142,3 @@ func _on_restart2_pressed() -> void:
 		
 		print("Game Over! Returning to main menu")
 		return  # exit the function so timer doesn't restar
-
-
-func update_lives_display() -> void:
-	$lifeContainer/lifeNumber.text = str(lives)
