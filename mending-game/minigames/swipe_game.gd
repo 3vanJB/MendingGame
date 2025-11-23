83249e9414959dd436c73f2b07400b358976d047
Jason A. Januar <jason.kwee98@gmail.com>
2025-11-23 11:40:15 -0800

    add randomizer to swipe game to make it a more proper game

--- mending-game/minigames/swipe_game.gd
+++ mending-game/minigames/swipe_game.gd
@@ -1,6 +1,13 @@
 extends Node
 
-@onready var swipable_object: SwipableObject = $SwipableObject
+@onready var swipable_object: SwipableObject = $Swiper/SwipableObject
+@onready var spawn_area: SpawnArea = $SpawnArea
+@onready var drop_zone: DropZone = $DropZone
+
+func _ready() -> void:
+	var random_point = spawn_area.get_random_vector()
+	spawn_area.queue_free()
+	drop_zone.global_position = random_point
 
 func _process(delta: float) -> void:
 	if swipable_object != null:
